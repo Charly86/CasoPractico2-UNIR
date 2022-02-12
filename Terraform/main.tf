140cd0fcc26a1683fe815f49aad1e3f1b35b3d6c
unknown <carvillarest@gmail.com>
2022-02-12 16:42:22 +0100

    creación ficheros terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.1"
+    }
+  }
+}
+
+# crea un service principal y rellena los siguientes datos para autenticar
+provider "azurerm" {
+  features {}
+   subscription_id = "edd8661f-2482-4fc3-bb09-aedf422d862d"
+   client_id = "6a555d21-391f-4092-bafb-aeb93a68c637" 
+   client_secret = "0A.es-LujHL-zHMPlToeO82kCk0owSrN98" 
+   tenant_id = "899789dc-202f-44b4-8472-a6d40f9eb440"
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+
+resource "azurerm_resource_group" "rg" {
+    name     =  "kubernetes_rg"
+    location = var.location
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Storage account
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
+
+resource "azurerm_storage_account" "stAccount" {
+    name                     = "staccountcp2" 
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
