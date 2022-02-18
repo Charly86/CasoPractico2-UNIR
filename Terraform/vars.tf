f20047d884f7069afe761ed349785ea73cb48fe5
unknown <carvillarest@gmail.com>
2022-02-18 16:11:03 +0100

    actualizaciones

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -7,25 +7,25 @@ variable "location" {
 #Se configura una maquina más potente para el master
 variable "vm_size_master" {
   type = string
-  description = "Tamaño de la máquina virtual master"
-  default = "Standard_B2s" # 4 GB, 2 CPU 
+  description = "Tamaño de la máquina virtual master y nfs"
+  default = "Standard_D2_v2" # 7 GB, 2 CPU 
 }
 
 #Se configuran maquinas menos potentes para los workers i nfs
-variable "vm_size_workers_nfs" {
+variable "vm_size_workers" {
   type = string
   description = "Tamaño de la máquina virtual workers y nfs"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  default = "Standard_A2_v2" # 4 GB, 2 CPU 
 }
 
 variable "vms_master" {
   type = list(string)
-  description = "Máquinas virtuales a crear como master"
+  description = "Máquinas virtuales a crear como master y nfs"
   default = ["master"]
 }
 
 variable "vms_workers_nfs" {
   type = list(string)
   description = "Máquinas virtuales a crear como workers y nfs"
-  default = ["worker01","worker02","nfs"]
+  default = ["worker01","worker02"]
 }
