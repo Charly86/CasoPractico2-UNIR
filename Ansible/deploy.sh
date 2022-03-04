836c43a90acacb855653c2dd8779dc69e48fa958
unknown <carvillarest@gmail.com>
2022-03-04 15:54:29 +0100

    cambios

--- Ansible/deploy.sh
+++ Ansible/deploy.sh
@@ -2,3 +2,5 @@
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
 ansible-playbook -i hosts -l deployKubernetes.yml
+ansible-playbook -i hosts -l deployNFS-Server.yml
+ansible-playbook -i hosts -l deployApp.yml
